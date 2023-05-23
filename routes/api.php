3b02c84e1f339e7b2bb8eddfd24b4c0ba188b0d5
ygiraldo-joonik <104454063+ygiraldo-joonik@users.noreply.github.com>
2023-05-23 13:59:07 -0500

    feat: call real camera server

--- routes/api.php
+++ routes/api.php
@@ -22,30 +22,42 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::get('/img', function (Request $request) {
-    error_log($request->query('new',0));
-    if ($request->query('new',0)) {
+    error_log(print_r($request->query('new', 0), true));
+    error_log(print_r($request->query('new', 0), true));
+    if ($request->query('new', 0) || !Storage::disk('local')->exists("cams/$request->port.jpg")) {
         $curl = curl_init();
 
-        $username = "username";
-        $password = "password";
+        $username = "Donaldedward1921";
+        $password = "Props&rotors@5755";
 
         curl_setopt_array($curl, array(
-            CURLOPT_URL => "http://185.10.80.33:8082/record/current.jpg",
+            // CURLOPT_URL => "http://185.10.80.33:8082/record/current.jpg",
+            CURLOPT_URL => "http://98.97.102.60:$request->port/record/current.jpg",
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_HTTPHEADER => array(
                 "Authorization: Basic " . base64_encode($username . ":" . $password)
             ),
+            CURLOPT_TIMEOUT_MS => 10000
         ));
 
         $response = curl_exec($curl);
         curl_close($curl);
 
         if ($response) {
-            Storage::disk('local')->put('cams/img.jpg', $response);
+            Storage::disk('local')->put("cams/$request->port.jpg", $response);
+        } else {
+            if (Storage::disk('local')->exists("cams/$request->port.jpg"))
+                return response()->file(storage_path("app/cams/$request->port.jpg"), [
+                    "cam-server-status" => "no response",
+                    "Access-Control-Expose-Headers"=>"cam-server-status" 
+                ]);
         }
     }
 
-    if (Storage::disk('local')->exists("cams/img.jpg"))
-        return response()->file(storage_path("app/cams/img.jpg"));
+    if (Storage::disk('local')->exists("cams/$request->port.jpg"))
+        return response()->file(storage_path("app/cams/$request->port.jpg", [
+            "cam-server-status" => "successful-response",
+            "Access-Control-Expose-Headers"=>"cam-server-status" 
+        ]));
     else return response()->status(404);
 });
